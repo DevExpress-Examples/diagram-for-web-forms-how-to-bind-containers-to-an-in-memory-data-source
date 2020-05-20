ffb8e1b86e221f284f9b0f556fd995395c8a96e1
dxstanley <>
2020-05-20 23:44:15 +0300

    add branch

--- CS/DiagramContainers/DiagramDataProvider.cs
+++ CS/DiagramContainers/DiagramDataProvider.cs
@@ -11,14 +11,14 @@ namespace DiagramContainers
     {
         public int ID { set; get; }
         public int? ContainerID { set; get; }
-        public int Type { set; get; }
+        public string Type { set; get; }
         public string Text { set; get; }
         public double X { set; get; }
         public double Y { set; get; }
         public double Width { set; get; }
         public double Height { set; get; }
 
-        public Item(int id, int? containerId, int type, string text, double x, double y, double width, double height)
+        public Item(int id, int? containerId, string type, string text, double x, double y, double width, double height)
         {
             ID = id;
             ContainerID = containerId;
@@ -43,20 +43,20 @@ namespace DiagramContainers
                 {
                     data = new List<Item>
                     {
-                        new Item(1, null, (int)DiagramShapeType.VerticalContainer, "Development", 0.5, 0.5, 7.25, 5),
-                        new Item(2, 1, (int)DiagramShapeType.VerticalContainer, "ASP.NET Team", 0.75, 1, 1.5, 4),
-                        new Item(3, 1, (int)DiagramShapeType.VerticalContainer, "JavaScript Team", 2.5, 1, 1.5, 4),
-                        new Item(4, 1, (int)DiagramShapeType.VerticalContainer, "WPF Team", 4.25, 1, 1.5, 4),
-                        new Item(5, 1, (int)DiagramShapeType.VerticalContainer, "WinForms Team", 6, 1, 1.5, 4),
-                        new Item(6, 2, (int)DiagramShapeType.Rectangle, "Laurence Lebihan", 1, 1.5, 1, 0.75),
-                        new Item(7, 2, (int)DiagramShapeType.Rectangle, "Elizabeth Lincoln", 1, 2.5, 1, 0.75),
-                        new Item(8, 3, (int)DiagramShapeType.Rectangle, "Patricio Simpson", 2.75, 1.5, 1, 0.75),
-                        new Item(9, 3, (int)DiagramShapeType.Rectangle, "Francisco Chang", 2.75, 2.5, 1, 0.75),
-                        new Item(10, 4, (int)DiagramShapeType.Rectangle, "Christina Berglund", 4.5, 1.5, 1, 0.75),
-                        new Item(11, 4, (int)DiagramShapeType.Rectangle, "Hanna Moos", 4.5, 2.5, 1, 0.75),
-                        new Item(12, 4, (int)DiagramShapeType.Rectangle, "Frederique Citeaux", 4.5, 3.5, 1, 0.75),
-                        new Item(13, 5, (int)DiagramShapeType.Rectangle, "Ana Trujillo", 6.25, 1.5, 1, 0.75),
-                        new Item(14, 5, (int)DiagramShapeType.Rectangle, "Antonio Moreno", 6.25, 2.5, 1, 0.75)
+                        new Item(1, null, DiagramShapeType.VerticalContainer.ToString(), "Development", 0.5, 0.5, 7.25, 5),
+                        new Item(2, 1, DiagramShapeType.VerticalContainer.ToString(), "ASP.NET Team", 0.75, 1, 1.5, 4),
+                        new Item(3, 1, DiagramShapeType.VerticalContainer.ToString(), "JavaScript Team", 2.5, 1, 1.5, 4),
+                        new Item(4, 1, DiagramShapeType.VerticalContainer.ToString(), "WPF Team", 4.25, 1, 1.5, 4),
+                        new Item(5, 1, DiagramShapeType.VerticalContainer.ToString(), "WinForms Team", 6, 1, 1.5, 4),
+                        new Item(6, 2, DiagramShapeType.Rectangle.ToString(), "Laurence Lebihan", 1, 1.5, 1, 0.75),
+                        new Item(7, 2, DiagramShapeType.Rectangle.ToString(), "Elizabeth Lincoln", 1, 2.5, 1, 0.75),
+                        new Item(8, 3, DiagramShapeType.Rectangle.ToString(), "Patricio Simpson", 2.75, 1.5, 1, 0.75),
+                        new Item(9, 3, DiagramShapeType.Rectangle.ToString(), "Francisco Chang", 2.75, 2.5, 1, 0.75),
+                        new Item(10, 4, DiagramShapeType.Rectangle.ToString(), "Christina Berglund", 4.5, 1.5, 1, 0.75),
+                        new Item(11, 4, DiagramShapeType.Rectangle.ToString(), "Hanna Moos", 4.5, 2.5, 1, 0.75),
+                        new Item(12, 4, DiagramShapeType.Rectangle.ToString(), "Frederique Citeaux", 4.5, 3.5, 1, 0.75),
+                        new Item(13, 5, DiagramShapeType.Rectangle.ToString(), "Ana Trujillo", 6.25, 1.5, 1, 0.75),
+                        new Item(14, 5, DiagramShapeType.Rectangle.ToString(), "Antonio Moreno", 6.25, 2.5, 1, 0.75)
                     };
                     HttpContext.Current.Session["DiagramNodes"] = data;
                 }
--- VB/DiagramContainers/DiagramDataProvider.vb
+++ VB/DiagramContainers/DiagramDataProvider.vb
@@ -9,14 +9,14 @@ Namespace DiagramContainers
 	Public Class Item
 		Public Property ID() As Integer
 		Public Property ContainerID() As Integer?
-		Public Property Type() As Integer
+		Public Property Type() As String
 		Public Property Text() As String
 		Public Property X() As Double
 		Public Property Y() As Double
 		Public Property Width() As Double
 		Public Property Height() As Double
 
-		Public Sub New(ByVal id As Integer, ByVal containerId? As Integer, ByVal type As Integer, ByVal text As String, ByVal x As Double, ByVal y As Double, ByVal width As Double, ByVal height As Double)
+		Public Sub New(ByVal id As Integer, ByVal containerId? As Integer, ByVal type As String, ByVal text As String, ByVal x As Double, ByVal y As Double, ByVal width As Double, ByVal height As Double)
 			Me.ID = id
 			Me.ContainerID = containerId
 			Me.Type = type
@@ -36,20 +36,20 @@ Namespace DiagramContainers
 				Dim data = TryCast(HttpContext.Current.Session("DiagramNodes"), List(Of Item))
 				If data Is Nothing Then
 					data = New List(Of Item) From {
-						New Item(1, Nothing, CInt(DiagramShapeType.VerticalContainer), "Development", 0.5, 0.5, 7.25, 5),
-						New Item(2, 1, CInt(DiagramShapeType.VerticalContainer), "ASP.NET Team", 0.75, 1, 1.5, 4),
-						New Item(3, 1, CInt(DiagramShapeType.VerticalContainer), "JavaScript Team", 2.5, 1, 1.5, 4),
-						New Item(4, 1, CInt(DiagramShapeType.VerticalContainer), "WPF Team", 4.25, 1, 1.5, 4),
-						New Item(5, 1, CInt(DiagramShapeType.VerticalContainer), "WinForms Team", 6, 1, 1.5, 4),
-						New Item(6, 2, CInt(DiagramShapeType.Rectangle), "Laurence Lebihan", 1, 1.5, 1, 0.75),
-						New Item(7, 2, CInt(DiagramShapeType.Rectangle), "Elizabeth Lincoln", 1, 2.5, 1, 0.75),
-						New Item(8, 3, CInt(DiagramShapeType.Rectangle), "Patricio Simpson", 2.75, 1.5, 1, 0.75),
-						New Item(9, 3, CInt(DiagramShapeType.Rectangle), "Francisco Chang", 2.75, 2.5, 1, 0.75),
-						New Item(10, 4, CInt(DiagramShapeType.Rectangle), "Christina Berglund", 4.5, 1.5, 1, 0.75),
-						New Item(11, 4, CInt(DiagramShapeType.Rectangle), "Hanna Moos", 4.5, 2.5, 1, 0.75),
-						New Item(12, 4, CInt(DiagramShapeType.Rectangle), "Frederique Citeaux", 4.5, 3.5, 1, 0.75),
-						New Item(13, 5, CInt(DiagramShapeType.Rectangle), "Ana Trujillo", 6.25, 1.5, 1, 0.75),
-						New Item(14, 5, CInt(DiagramShapeType.Rectangle), "Antonio Moreno", 6.25, 2.5, 1, 0.75)
+						New Item(1, Nothing, DiagramShapeType.VerticalContainer.ToString(), "Development", 0.5, 0.5, 7.25, 5),
+						New Item(2, 1, DiagramShapeType.VerticalContainer.ToString(), "ASP.NET Team", 0.75, 1, 1.5, 4),
+						New Item(3, 1, DiagramShapeType.VerticalContainer.ToString(), "JavaScript Team", 2.5, 1, 1.5, 4),
+						New Item(4, 1, DiagramShapeType.VerticalContainer.ToString(), "WPF Team", 4.25, 1, 1.5, 4),
+						New Item(5, 1, DiagramShapeType.VerticalContainer.ToString(), "WinForms Team", 6, 1, 1.5, 4),
+						New Item(6, 2, DiagramShapeType.Rectangle.ToString(), "Laurence Lebihan", 1, 1.5, 1, 0.75),
+						New Item(7, 2, DiagramShapeType.Rectangle.ToString(), "Elizabeth Lincoln", 1, 2.5, 1, 0.75),
+						New Item(8, 3, DiagramShapeType.Rectangle.ToString(), "Patricio Simpson", 2.75, 1.5, 1, 0.75),
+						New Item(9, 3, DiagramShapeType.Rectangle.ToString(), "Francisco Chang", 2.75, 2.5, 1, 0.75),
+						New Item(10, 4, DiagramShapeType.Rectangle.ToString(), "Christina Berglund", 4.5, 1.5, 1, 0.75),
+						New Item(11, 4, DiagramShapeType.Rectangle.ToString(), "Hanna Moos", 4.5, 2.5, 1, 0.75),
+						New Item(12, 4, DiagramShapeType.Rectangle.ToString(), "Frederique Citeaux", 4.5, 3.5, 1, 0.75),
+						New Item(13, 5, DiagramShapeType.Rectangle.ToString(), "Ana Trujillo", 6.25, 1.5, 1, 0.75),
+						New Item(14, 5, DiagramShapeType.Rectangle.ToString(), "Antonio Moreno", 6.25, 2.5, 1, 0.75)
 					}
 					HttpContext.Current.Session("DiagramNodes") = data
 				End If
